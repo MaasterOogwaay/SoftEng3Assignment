2efbe04db9ca5f977660b5549de5b7c6e05c55c3
jt181 <152308253+jt181@users.noreply.github.com>
2024-10-02 12:22:46 +0100

    james is ready

--- Code/src/newsagent/HelloWorld.java
+++ Code/src/newsagent/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World! First Player Ready");
 		System.out.println("Second Player Ready");
+		System.out.println("Third Player Ready");
 	}
 
 }
