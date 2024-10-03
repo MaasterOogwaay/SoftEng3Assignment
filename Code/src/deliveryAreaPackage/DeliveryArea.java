48c3635cdb957e944920aeb3e282892167dbf58e
DarraghAI835 <119582457+DarraghAI835@users.noreply.github.com>
2024-10-04 00:56:24 +0100

    Added skeletons for publications and areas

---
+++ Code/src/deliveryAreaPackage/DeliveryArea.java
@@ -0,0 +1,26 @@
+package deliveryAreaPackage;
+
+public class DeliveryArea {
+	private Driver DeliveryDriver;
+	private String description;
+	
+	public DeliveryArea() {}
+	
+	public Driver DeliveryDriver() {}
+	
+	public void setDriver() {}
+	
+	public Driver updateDriver() {}
+	
+	public String getDescription() {}
+	
+	public void setDescription() {}
+	
+	public String updateDescrioption() {}
+	
+	public static void printArea() {}
+	
+	public static void printAllAreas() {}
+	
+	public static void deleteArea() {}
+}
