ee3b414eadcac7cdb2e9e71fae346e9cf923dd12
Dylan Thomas <dylanthomas1108@gmail.com>
2024-10-03 21:42:54 +0100

    dylans UML

---
+++ Code/src/deliveryDocketPackage/DeliveryDocket.java
@@ -0,0 +1,21 @@
+package deliveryDocketPackage;
+
+public class DeliveryDocket {
+	
+	private int docketId;
+	
+	public DeliveryDocket() {}
+	
+	public int getDocketId() {}
+	
+	public void setDocketId() {}
+	
+	public static void generateDocket() {}
+	
+	public static void printDocket() {}
+	
+	public static void updateDocket() {}
+	
+	public static void deleteDocket() {}
+
+}
---
+++ Code/src/newsAgentPackage/NewsAgent.java
@@ -0,0 +1,22 @@
+package newsAgentPackage;
+
+public class NewsAgent {
+	
+	private String username;
+	private String password;
+	
+	public NewsAgent() {}
+	
+	public String getUserName() {}
+	
+	public void setUsername() {}
+	
+	public String getPassWord() {}
+	
+	public void setPassWord() {}
+	
+	public static void validateUserName() {}
+	
+	public static void validatePassWord() {}
+
+}
