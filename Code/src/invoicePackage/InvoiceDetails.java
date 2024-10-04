7b3057a65b69c15e900d011dd8bbaa8b8d86a5e4
jt181 <152308253+jt181@users.noreply.github.com>
2024-10-04 01:40:37 +0100

    creating uml for invoices and orders

---
+++ Code/src/invoicePackage/InvoiceDetails.java
@@ -0,0 +1,5 @@
+package invoicePackage;
+
+public class InvoiceDetails {
+
+}
---
+++ Code/src/odersPackage/OderDetails.java
@@ -0,0 +1,14 @@
+package odersPackage;
+
+public class OderDetails {
+
+	private int orderId;
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+}
